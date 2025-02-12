4df1f018720f5bba300c2b17a00b9309c883c4b7
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2025-02-12 11:35:30 +0100

    Brain robot 1.0
    Faire le tour de l'arène

---
+++ src/algorithms/BrainTest1.java
@@ -0,0 +1,72 @@
+/* ******************************************************
+ * Simovies - Eurobot 2015 Robomovies Simulator.
+ * Copyright (C) 2014 <Binh-Minh.Bui-Xuan@ens-lyon.org>.
+ * GPL version>=3 <http://www.gnu.org/licenses/>.
+ * $Id: algorithms/BrainCanevas.java 2014-10-19 buixuan.
+ * ******************************************************/
+package algorithms;
+
+import characteristics.Parameters;
+import robotsimulator.Brain;
+import characteristics.IFrontSensorResult;
+
+public class BrainTest1 extends Brain {
+
+    private int taskStep;
+    private final double epsilon = 0.05;
+    private static final IFrontSensorResult.Types WALL = IFrontSensorResult.Types.WALL;
+
+    public BrainTest1() {
+        super();
+    }
+
+    public void activate() {
+        taskStep = 0;
+    }
+
+    public void step() {
+        double botHeading = getHeading() % (2 * Math.PI);
+        double parameterOrientation = 0;
+
+        switch (taskStep) {
+            case 0:
+                parameterOrientation = Parameters.NORTH;
+                break;
+            case 1:
+                parameterOrientation = Parameters.EAST;
+                break;
+            case 2:
+                parameterOrientation = Parameters.SOUTH;
+                break;
+            case 3:
+                parameterOrientation = Parameters.WEST;
+                break;
+            case 4:
+                return; // Fin du parcours
+        }
+
+        // Ajustement pour prendre en compte les angles négatifs correctement
+        if (parameterOrientation < 0) {
+            parameterOrientation += 2 * Math.PI;
+        }
+        if (botHeading < 0) {
+            botHeading += 2 * Math.PI;
+        }
+
+        double headMinus = parameterOrientation - epsilon;
+        double headPlus = parameterOrientation + epsilon;
+
+        sendLogMessage(taskStep + " / M : " + headMinus + " / P : " + headPlus + " / O : " + botHeading);
+
+        if (botHeading >= headMinus && botHeading <= headPlus) {
+            if (detectFront().getObjectType() == WALL) {
+                taskStep++; // Incrémentation seulement si un mur est détecté
+                stepTurn(Parameters.Direction.RIGHT); // Tourne à droite avant de continuer
+            } else {
+                move();
+            }
+        } else {
+            stepTurn(Parameters.Direction.RIGHT);
+        }
+    }
+}
